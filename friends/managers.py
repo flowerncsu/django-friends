1d114321eacc58c5b468edee45b66294ab47d5d1
Charlotte Mays <charlotte.ann.mays@gmail.com>
2018-08-18 12:31:33 -0400

    revert unneeded change

--- friends/managers.py
+++ friends/managers.py
@@ -14,11 +14,11 @@ class FriendshipManager(models.Manager):
     def friends_for_user(self, user):
         friends = []
         qs = self.filter(Q(from_user=user) | Q(to_user=user)).select_related('from_user', 'to_user')
-        for elem in qs:
-            if elem.from_user == user:
-                friends.append({"friend": elem.to_user, "friendship": elem})
+        for friendship in qs:
+            if friendship.from_user == user:
+                friends.append({"friend": friendship.to_user, "friendship": friendship})
             else:
-                friends.append({"friend": elem.from_user, "friendship": elem})
+                friends.append({"friend": friendship.from_user, "friendship": friendship})
         return friends
 
     def are_friends(self, user1, user2):
